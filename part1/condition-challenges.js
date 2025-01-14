8a4faf48d588664e232f8c849c2f9baea04be08c
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-01-14 21:18:47 +0530

    condition-challenges

---
+++ part1/condition-challenges.js
@@ -0,0 +1,62 @@
+// checking if a number is greater than other number
+
+let num1 = 10;
+let num2 = 20;
+
+/*
+console.log("start of the program");
+if (num1 > num2) {
+    console.log("num 1 is the great than num 2"); 
+} else {
+    console.log("nope , num 1 is not greater than num 2");
+    
+}
+
+console.log("End of the program"); 
+*/
+
+// checking if a string is equal to another string:
+
+let username = "admin";
+let anotherUsername = "admin";
+
+/*
+if (username == anotherUsername) {
+    console.log("pick another username");
+} else { 
+    console.log("pick other username");
+}; 
+*/
+
+// Checking if a variable is a number or not:
+
+let score = "44";
+/*
+if (typeof score === "number") {
+    console.log("score is a number");
+} else {
+    console.log("score is not a number");
+}
+*/
+
+// Checking if a boolean value is true or false:
+
+let isTeaReady = false;
+
+if (isTeaReady) {
+    console.log("yes, tea is ready");
+} else {
+  //  console.log("no, tea is not ready");
+}
+
+// checking if an array is empty or not:
+
+let items = ["ariz", "john", "jane"];
+
+console.log(items.length);
+
+if (items.length === 0) {
+    console.log("array is empty");
+} else {
+    console.log("array is NOT empty");
+}
